6ffaaa552e67f48d95ae2aeb792710e87678aa78
Ramil <alimuahmed.ramil@gmail.com>
2024-09-07 22:24:50 +0300

    [10] feature: Добавлен шаблон микросервиса локации.

---
+++ services/Location/Location.Infrastructure/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Location.Infrastructure;
+
+public class Class1
+{
+}
---
+++ services/Location/Location.WebApi.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Location.WebApi.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
