a0054da220a90b4f8d9e38a2d8db1a45445b72f7
Ramil <alimuahmed.ramil@gmail.com>
2024-09-19 19:34:23 +0300

    Рефакторинг.

--- services/order/Ali.Delivery.Order.Domain/Entities/Order.cs
+++ services/order/Ali.Delivery.Order.Domain/Entities/Order.cs
@@ -1,26 +1,39 @@
 ﻿using Ali.Delivery.Domain.Core;
 using Ali.Delivery.Domain.Core.Primitives;
+using Ali.Delivery.Order.Domain.ValueObjects.Order;
 
 namespace Ali.Delivery.Order.Domain.Entities;
 
 public class Order : Entity<SequentialGuid>
 {
     /// <summary>
-    /// Инициализирует новый экземпляр типа <see cref="T:Ali.Delivery.Domain.Core.Entity`1" />.
+    /// Инициализирует новый экземпляр типа <see cref="Order" />.
     /// </summary>
     /// <param name="id">Идентификатор.</param>
-    /// <exception cref="T:System.ArgumentNullException">
-    /// Возникает, если <paramref name="id" /> равен <c>null</c>.
+    /// <param name="name">Наименование цели.</param>
+    /// <exception cref="ArgumentNullException">
+    /// Возникает, если <paramref name="name" /> или
+    /// <paramref name="name" /> равен <c>null</c>.
     /// </exception>
-    public Order(SequentialGuid id)
+    public Order(SequentialGuid id, OrderName name)
         : base(id)
     {
+        Name = name ?? throw new ArgumentNullException(nameof(name));
     }
 
+    /// <summary>
+    /// Инициализирует новый экземпляр типа <see cref="Order" />.
+    /// </summary>
+    /// <param name="id">Идентификатор.</param>
+    /// <remarks>Конструктор для EF.</remarks>
     protected Order()
         : base(SequentialGuid.Empty)
     {
+        Name = null!;
     }
 
-    public bool Test { get; }
+    /// <summary>
+    /// Возвращает наименование заказа.
+    /// </summary>
+    public OrderName Name { get; private set; }
 }
--- services/order/Ali.Delivery.Order.Infrastructure/Persistence/Configurations/OrderConfiguration.cs
+++ services/order/Ali.Delivery.Order.Infrastructure/Persistence/Configurations/OrderConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using Ali.Delivery.Order.Infrastructure.Persistence.Configurations.Base;
+﻿using Ali.Delivery.Order.Domain.ValueObjects.Order;
+using Ali.Delivery.Order.Infrastructure.Persistence.Configurations.Base;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -17,7 +18,10 @@ internal class CourierConfiguration : EntityTypeConfigurationBase<Domain.Entitie
     {
         builder.ToTable("orders", t => t.HasComment("Заказ"));
 
-        builder.Property(p => p.Test)
-               .HasComment("Test");
+        builder.Property(p => p.Name)
+               .IsRequired()
+               .HasMaxLength(OrderName.MaxLength)
+               .HasComment("Наименование заказа")
+               .HasConversion(o => (string)o, s => new OrderName(s));
     }
 }
