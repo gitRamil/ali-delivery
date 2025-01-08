a34ea3feb855525407d54db9a2ce7c9ac15489b6
Ramil <alimuahmed.ramil@gmail.com>
2025-01-09 00:21:25 +0300

    Тестовое изменение

--- services/order/Ali.Delivery.Order.WebApi/Controllers/OrderController.cs
+++ services/order/Ali.Delivery.Order.WebApi/Controllers/OrderController.cs
@@ -12,7 +12,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Ali.Delivery.Order.WebApi.Controllers;
 
 /// <summary>
-/// Контроллер для управления действиями с заказами.
+/// Контроллер для управления действиями с заказами.2
 /// </summary>
 [ApiController]
 [Route("api/v{version:apiVersion}/[controller]")]
