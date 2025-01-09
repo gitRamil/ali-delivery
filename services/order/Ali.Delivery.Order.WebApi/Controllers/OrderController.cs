75b39525eec57a6b68a06a3079253d137e5abab1
Ramil <alimuahmed.ramil@gmail.com>
2025-01-09 03:42:35 +0300

    Фикс конфига

--- services/order/Ali.Delivery.Order.WebApi/Controllers/OrderController.cs
+++ services/order/Ali.Delivery.Order.WebApi/Controllers/OrderController.cs
@@ -12,7 +12,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Ali.Delivery.Order.WebApi.Controllers;
 
 /// <summary>
-/// Контроллер для управления действиями с заказами.4
+/// Контроллер для управления действиями с заказами.5
 /// </summary>
 [ApiController]
 [Route("api/v{version:apiVersion}/[controller]")]
