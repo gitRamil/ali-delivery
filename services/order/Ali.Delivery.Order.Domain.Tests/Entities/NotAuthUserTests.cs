953a8582e1e965af7f521b55597b3621c4cdd4e5
Ramil <RAlimukhamedov@inno.tech>
2025-03-15 09:46:01 +0300

    [43] issue: Шаблон сто процентного покрытия тестами.

--- services/order/Ali.Delivery.Order.Domain.Tests/Entities/NotAuthUserTests.cs
+++ services/order/Ali.Delivery.Order.Domain.Tests/Entities/NotAuthUserTests.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Ali.Delivery.Domain.Core.Primitives;
 using Ali.Delivery.Order.Domain.Entities;
 using Ali.Delivery.Order.Domain.Entities.Dictionaries;
@@ -11,35 +12,6 @@ namespace Ali.Delivery.Order.Domain.Tests.Entities;
 public class NotAuthUserTests
 {
     [Fact]
-    public void CreateNotAuthUserShouldSucceedWhenAllValidArgumentsPassed()
-    {
-        // Arrange.
-        var fixture = new Fixture();
-        var id = fixture.Create<SequentialGuid>();
-        var creatorPassportInfo = new PassportInfo(SequentialGuid.Create(),
-                                                   PassportType.Internal,
-                                                   new PassportInfoPassportNumber("123456789"),
-                                                   new PassportInfoRegDate(DateTime.Now),
-                                                   new PassportInfoIssuedBy("MVD RF"));
-        var creator = new User(fixture.Create<SequentialGuid>(),
-                               fixture.Create<UserLogin>(),
-                               fixture.Create<UserPassword>(),
-                               fixture.Create<Role>(),
-                               fixture.Create<UserBirthDay>(),
-                               fixture.Create<UserFirstName>(),
-                               fixture.Create<UserLastName>(),
-                               creatorPassportInfo);
-        var firstName = fixture.Create<NotAuthUserFirstName>();
-        var lastName = fixture.Create<NotAuthUserLastName>();
-        var phoneNumber = new NotAuthUserPhoneNumber("+77877777777");
-        
-        // Act.
-        var act = () => new NotAuthUser(id, creator, firstName, lastName, phoneNumber);
-       
-        // Assert.
-        act.Should().NotThrow();
-    }
-    [Fact]
     public void CreateNotAuthUserShouldSThrowArgumentNullExceptionWhenCreatorIsNull()
     {
         // Arrange.
@@ -49,12 +21,172 @@ public class NotAuthUserTests
         var firstName = fixture.Create<NotAuthUserFirstName>();
         var lastName = fixture.Create<NotAuthUserLastName>();
         var phoneNumber = new NotAuthUserPhoneNumber("+77877777777");
-        
+
         // Act.
         var act = () => new NotAuthUser(id, creator, firstName, lastName, phoneNumber);
-       
+
         // Assert.
         act.Should()
            .Throw<ArgumentNullException>(nameof(creator));
+    }
+
+    [Fact]
+    public void CreateNotAuthUserShouldSThrowArgumentNullExceptionWhenFirstNameIsNull()
+    {
+        // Arrange.
+        var fixture = new Fixture();
+        var id = fixture.Create<SequentialGuid>();
+        var creator = CreateUser();
+        NotAuthUserFirstName firstName = null!;
+        var lastName = fixture.Create<NotAuthUserLastName>();
+        var phoneNumber = new NotAuthUserPhoneNumber("+77877777777");
+
+        // Act.
+        var act = () => new NotAuthUser(id, creator, firstName, lastName, phoneNumber);
+
+        // Assert.
+        act.Should()
+           .Throw<ArgumentNullException>(nameof(firstName));
+    }
+
+    [Fact]
+    public void CreateNotAuthUserShouldSThrowArgumentNullExceptionWhenLastNameIsNull()
+    {
+        // Arrange.
+        var fixture = new Fixture();
+        var id = fixture.Create<SequentialGuid>();
+        var creator = CreateUser();
+        var firstName = fixture.Create<NotAuthUserFirstName>();
+        NotAuthUserLastName lastName = null!;
+        var phoneNumber = new NotAuthUserPhoneNumber("+77877777777");
+
+        // Act.
+        var act = () => new NotAuthUser(id, creator, firstName, lastName, phoneNumber);
+
+        // Assert.
+        act.Should()
+           .Throw<ArgumentNullException>(nameof(lastName));
+    }
+
+    [Fact]
+    public void CreateNotAuthUserShouldSThrowArgumentNullExceptionWhenPhoneNumberIsNull()
+    {
+        // Arrange.
+        var fixture = new Fixture();
+        var id = fixture.Create<SequentialGuid>();
+        var creator = CreateUser();
+        var firstName = fixture.Create<NotAuthUserFirstName>();
+        var lastName = fixture.Create<NotAuthUserLastName>();
+        NotAuthUserPhoneNumber phoneNumber = null!;
+
+        // Act.
+        var act = () => new NotAuthUser(id, creator, firstName, lastName, phoneNumber);
+
+        // Assert.
+        act.Should()
+           .Throw<ArgumentNullException>(nameof(phoneNumber));
+    }
+
+    [Fact]
+    public void CreateNotAuthUserShouldSucceedSetAllProperties()
+    {
+        // Arrange.
+        var fixture = new Fixture();
+        var id = fixture.Create<SequentialGuid>();
+        var creator = CreateUser();
+        var firstName = fixture.Create<NotAuthUserFirstName>();
+        var lastName = fixture.Create<NotAuthUserLastName>();
+        var phoneNumber = new NotAuthUserPhoneNumber("+77877777777");
+
+        // Act.
+        var notAuthUser = new NotAuthUser(id, creator, firstName, lastName, phoneNumber);
+
+        // Assert.
+        notAuthUser.Should()
+                   .NotBeNull();
+
+        notAuthUser.Id.Should()
+                   .Be(id);
+
+        notAuthUser.Creator.Should()
+                   .Be(creator);
+
+        notAuthUser.FirstName.Should()
+                   .Be(firstName);
+
+        notAuthUser.LastName.Should()
+                   .Be(lastName);
+
+        notAuthUser.PhoneNumber.Should()
+                   .Be(phoneNumber);
+    }
+
+    [Fact]
+    public void CreateNotAuthUserShouldSucceedWhenAllValidArgumentsPassed()
+    {
+        // Arrange.
+        var fixture = new Fixture();
+        var id = fixture.Create<SequentialGuid>();
+        var creator = CreateUser();
+        var firstName = fixture.Create<NotAuthUserFirstName>();
+        var lastName = fixture.Create<NotAuthUserLastName>();
+        var phoneNumber = new NotAuthUserPhoneNumber("+77877777777");
+
+        // Act.
+        var act = () => new NotAuthUser(id, creator, firstName, lastName, phoneNumber);
+
+        // Assert.
+        act.Should()
+           .NotThrow();
+    }
+
+    [Fact]
+    public void ProtectedConstructorShouldInitializePropertiesWithDefaultValues()
+    {
+        // Arrange
+        var type = typeof(NotAuthUser);
+        var constructor = type.GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic, null, Type.EmptyTypes, null)!;
+
+        // Act
+        var notAuthUser = (NotAuthUser)constructor.Invoke(null);
+
+        // Assert
+        notAuthUser.Should()
+                   .NotBeNull();
+
+        notAuthUser.Id.Should()
+                   .Be(SequentialGuid.Empty);
+
+        notAuthUser.Creator.Should()
+                   .BeNull();
+
+        notAuthUser.FirstName.Should()
+                   .BeNull();
+
+        notAuthUser.LastName.Should()
+                   .BeNull();
+
+        notAuthUser.PhoneNumber.Should()
+                   .BeNull();
+    }
+
+    private static User CreateUser()
+    {
+        var fixture = new Fixture();
+
+        var creatorPassportInfo = new PassportInfo(SequentialGuid.Create(),
+                                                   PassportType.Internal,
+                                                   new PassportInfoPassportNumber("123456789"),
+                                                   new PassportInfoRegDate(DateTime.Now),
+                                                   new PassportInfoIssuedBy("MVD RF"));
+
+        return new User(fixture.Create<SequentialGuid>(),
+                        fixture.Create<UserLogin>(),
+                        fixture.Create<UserPassword>(),
+                        fixture.Create<Role>(),
+                        fixture.Create<UserBirthDay>(),
+                        fixture.Create<UserFirstName>(),
+                        fixture.Create<UserLastName>(),
+                        creatorPassportInfo);
     }
 }
