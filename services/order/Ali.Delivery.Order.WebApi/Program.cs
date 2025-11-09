9a1d87a1bdb7fe58c4c7067fdb721271814ba985
Ramil <alimuahmed.ramil@gmail.com>
2025-11-09 21:25:36 +0300

    Рабочая версия кубер манифестов

--- services/order/Ali.Delivery.Order.WebApi/Program.cs
+++ services/order/Ali.Delivery.Order.WebApi/Program.cs
@@ -44,7 +44,6 @@ try
     builder.Services.AddRabbitMqService(builder.Configuration);
 
     var app = builder.Build();
-    app.UseHttpsRedirection();
     app.AddAutomaticMigrations();
 
     var consumer = app.Services.GetRequiredService<IMessageConsumer>();
